f2c42c142403e4701bf01c9471dd8e9fdd93d8af
Laurent Chasserat <laurent.chasserat@gmail.com>
2019-01-16 09:05:42 +0100

    Draw little legend - NOTE : Server crashed periodically...

--- Workspace/UserInterface/WebContent/js/smartBuilding.js
+++ Workspace/UserInterface/WebContent/js/smartBuilding.js
@@ -1,13 +1,14 @@
-//setTimeout("window.location.reload()",10000);
+//setTimeout("window.location.reload()",3000);
 
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
 var img = new Image();
 img.onload = function() {
-	console.log("Drawing image");
+	console.log("Drawing image.");
 	ctx.drawImage(img, 0, 0);
-	
+
+	console.log("Drawing sensors.");
 	var tempSensorsNb = parseInt($("#tsnb").text());
 	for (var i=0; i<tempSensorsNb; i++) {
 		var tempSensorsId = $("#tsid"+(i.toString())).text();
@@ -19,6 +20,9 @@ img.onload = function() {
 		drawTempSensor(x, y, i);
 		
 	}
+
+	console.log("Drawing legend.");
+	drawLegend();
 };
 img.src = "img/plan.png";50
 
@@ -39,15 +43,47 @@ canvas.addEventListener('click', function(evt) {
 }, false);
 
 function drawTempSensor(x, y, nb) {
+	drawSensor(x, y, nb, 'rgb(51, 204, 51, 0.8)', 'rgb(20, 82, 20, 0.8)');
+}
+
+function drawLumSensor(x, y, nb) {
+	drawSensor(x, y, nb, 'rgb(255, 255, 26, 0.8)', 'rgb(179, 179, 0, 0.8)');
+}
+
+function drawSensor(x, y, nb, color1, color2) {
 	ctx.beginPath();
 	ctx.arc(x, y, 20, 0, 2 * Math.PI, false);
-	ctx.fillStyle = 'rgb(51, 204, 51, 0.8)';
+	ctx.fillStyle = color1; //'rgb(51, 204, 51, 0.8)';
 	ctx.fill();
 	ctx.lineWidth = 5;
-	ctx.strokeStyle = 'rgb(20, 82, 20, 0.8)';
+	ctx.strokeStyle = color2; //'rgb(20, 82, 20, 0.8)';
 	ctx.stroke();
 
-	ctx.fillStyle = 'rgb(20, 82, 20, 0.8)';
+	ctx.fillStyle = color2 //'rgb(20, 82, 20, 0.8)';
 	ctx.font = "16px Arial";
 	ctx.fillText(nb.toString(), x-4, y+6); 
+}
+
+function drawLegend() {
+	var c = document.getElementById("myCanvas");
+	var ctx = c.getContext("2d");
+	ctx.fillStyle = 'rgb(0, 0, 0, 0.1)';
+	ctx.fillRect(20, 20, 200, 200); 
+	
+	// Title
+	ctx.fillStyle = 'rgb(0,0,0)';
+	ctx.font = "bold 16px Arial";
+	ctx.fillText("Capteurs :", 30, 40); 
+	
+	// Temp sensor
+	ctx.fillStyle = 'rgb(0,0,0)';
+	ctx.font = "16px Arial";
+	ctx.fillText("Température :", 40, 70); 	
+	drawTempSensor(180, 65, 0);
+	
+	// Lum sensor
+	ctx.fillStyle = 'rgb(0,0,0)';
+	ctx.font = "16px Arial";
+	ctx.fillText("Luminosité :", 40, 125); 
+	drawLumSensor(180, 120, 0);
 }
